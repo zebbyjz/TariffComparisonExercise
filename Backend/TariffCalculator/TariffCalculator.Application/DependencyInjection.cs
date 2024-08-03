87616e1647b11cd3552832bccdf5e240d05ca669
Jehanzaib Rajput <megajehanzaib@gmail.com>
2024-08-04 03:16:25 +0500

    Created Backend Project structure
    -Created Projects according to clean architecture
    -Added some basic DI extension methods
    -Added FluentValidation NuGet package

---
+++ Backend/TariffCalculator/TariffCalculator.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TariffCalculator.Application
+{
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Extension method for injecting dependencies relevant to application layer.
+        /// <br></br>
+        /// To be called from Entry point of WebAPI.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddApplication (this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+            return services;
+        }
+    }
+}
---
+++ Backend/TariffCalculator/TariffCalculator.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TariffCalculator.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Extension method for injecting dependencies relevant to Infrastructure layer like external services and Database access.
+        /// <br></br>
+        /// To be called from Entry point of WebAPI.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddInfrastructure (this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
