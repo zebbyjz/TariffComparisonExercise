5e1ff42ed0a5b343826b31935db00e0d07ede74b
Jehanzaib Rajput <megajehanzaib@gmail.com>
2024-08-05 05:00:56 +0500

    Updated WebAPI program.cs
    -Wired Dependencies for Application and Infrastructure projects

--- Backend/TariffCalculator/TariffCalculator.API/Program.cs
+++ Backend/TariffCalculator/TariffCalculator.API/Program.cs
@@ -1,4 +1,7 @@
 
+using TariffCalculator.Application;
+using TariffCalculator.Infrastructure;
+
 namespace TariffCalculator.API
 {
     public class Program
@@ -9,6 +12,8 @@ namespace TariffCalculator.API
 
             // Add services to the container.
 
+            builder.Services.AddApplication();
+            builder.Services.AddInfrastructure();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
