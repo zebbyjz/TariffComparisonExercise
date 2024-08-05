0a0f9ef120be8a41766a8fe25ff8b14e3eba46e0
Jehanzaib Rajput <megajehanzaib@gmail.com>
2024-08-05 06:24:17 +0500

    Added API response messages

---
+++ Backend/TariffCalculator/TariffCalculator.API/ResponseMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace TariffCalculator.API;
+
+public static class ResponseMessages
+{
+    public static readonly string YearlyUsageCannotBeLessThanZero = "Yearly usage cannot be less than zero.";
+    public static readonly string InternalServerError = "An unexpected error occurred.";
+}
