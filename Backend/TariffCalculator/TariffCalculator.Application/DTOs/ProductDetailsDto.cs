13d29744e2bb7b03211158ac10fbd7c55711fe27
Jehanzaib Rajput <megajehanzaib@gmail.com>
2024-08-05 05:27:40 +0500

    Added DTO and extension method for ProductDetails
    -Added ProductDetailsDto
    -Added ToProductDetailsListDto extension method

---
+++ Backend/TariffCalculator/TariffCalculator.Application/DTOs/ProductDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TariffCalculator.Application.DTOs;
+
+public class ProductDetailsDto
+{
+    public string Name { get; set; }
+    public decimal BaseCost { get; set; }
+    public decimal AdditionalKwhCost { get; set; }
+    
+    public decimal YearlyCost { get; set; }
+}
---
+++ Backend/TariffCalculator/TariffCalculator.Application/Extensions/TariffExtensions.cs
@@ -0,0 +1,21 @@
+﻿using TariffCalculator.Application.DTOs;
+using TariffCalculator.Domain.Entities;
+using TariffCalculator.Domain.ServicesResult;
+
+namespace TariffCalculator.Application.Extensions;
+
+public static class TariffExtensions
+{
+    public static List<ProductDetailsDto> ToProductDetailsListDto(this List<ProductDetailsResult> productDetails)
+    {
+        return productDetails
+        .Select(productDetail => new ProductDetailsDto()
+        {
+            Name = productDetail.Name,
+            BaseCost = productDetail.BaseCost,
+            AdditionalKwhCost = productDetail.AdditionalKwhCost,
+            YearlyCost = productDetail.YearlyCost
+        })
+        .ToList();
+    }
+}
